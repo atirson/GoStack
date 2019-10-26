a972505568618ff6c951217fe2ca6fa15d4e8375
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2019-10-26 17:21:36 -0300

    feature: Add sends emails with Nodemailer and to format of emails with HandleBars.

---
+++ modulo-node/GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0cec263e980fd3',
+    pass: 'eebaa968e1f07f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
