48c8fac6c823409ef92747dec36c0019adcba94a
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2019-12-17 17:53:24 -0300

    chore: Added set up for react-native.

---
+++ react-native/modulo06/src/config/ReactotronConfig.js
@@ -0,0 +1,9 @@
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({ host: '10.0.0.135' })
+    .useReactNative()
+    .connect();
+
+  tron.clear();
+}
