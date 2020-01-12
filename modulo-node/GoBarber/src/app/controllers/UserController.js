3a92b59db3b4889a4beb882de4d72f1ce8cda577
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2020-01-11 22:41:33 -0300

    chore: Added info about File at the Session and User.

--- modulo-node/GoBarber/src/app/controllers/UserController.js
+++ modulo-node/GoBarber/src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserContoller {
   async store(req, res) {
@@ -68,13 +69,23 @@ class UserContoller {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      inclued: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 }
