2c485c2a030ff8c40dd6a47cdf470c4fc40ba855
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2020-01-15 08:53:53 -0300

    chore: Added config of Redux and actions for signIn and user.

--- modulo-node/reactgo/src/config/ReactotronConfig.js
+++ modulo-node/reactgo/src/config/ReactotronConfig.js
@@ -1,7 +1,12 @@
 import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().connect({ host: '10.0.0.135' });
+  const tron = Reactotron.configure()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect({ host: '10.0.0.135' });
 
   tron.clear();
 
