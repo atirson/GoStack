b7b09e94101ad021ff3da14ffcd4166aec3b75a2
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2020-01-17 16:50:36 -0300

    bug: Error forward 'Element type is invalid'.

---
+++ modulo-node/reactmobile/src/pages/SignIn/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Background from '~/components/Background';
+import Input from '~/components/Input';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Text>SingIn</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+    </Background>
+  );
+}
